6f18e27ea802958634be306f1e636b5c21989057
Alexgoated22 <alexanderhernandezreal@gmail.com>
2025-10-21 23:09:04 -0700

    Hour 7 p2

---
+++ Ahernandez_Hour7_P2/Assets/Scripts/MyBirthday.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBirthday : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int day = 1; day <= 31; day++)
+        {
+            if (day == 22)
+            {
+                Debug.Log("its my birthday!");
+
+            }
+            else
+            {
+                Debug.Log(day);
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
