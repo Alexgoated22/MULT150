db6687827f7b0873b4fba6bbd3e9fee88048d78b
Alexgoated22 <alexanderhernandezreal@gmail.com>
2025-10-21 22:17:56 -0700

    hour 7 p1

---
+++ Ahernandez_Hour7_P1/Assets/Scenes/Scripts/HelloWorldScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+public class HelloWorldScript : MonoBehaviour
+{
+    // Use this for intialization
+    void Start ()
+    {
+        print("Hello World");
+
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+
+
+    }
+}
