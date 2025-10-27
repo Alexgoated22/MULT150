a7cbcacd20b2dbf952104cdb62ac4a057ea8f827
Alexgoated22 <alexanderhernandezreal@gmail.com>
2025-10-26 21:48:05 -0700

    Hour 8 p1

---
+++ Ahernandez_Hour8__P1/Assets/Scenes/Scripts/FireBallScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int x = TakeDamageFromFireball();
+        print("Player health: " + x);
+
+        int y = TakeDamageFromFireball(25);
+        print("Player health: " + y);
+
+        int z = TakeDamageFromFireball(30, 75);
+        print("Player health: " + z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    int TakeDamageFromFireball()
+    {
+        int health = 100;
+        int damage = 10;
+        return health - damage;
+    }
+    int TakeDamageFromFireball(int damage)
+    {
+        int health = 100;
+        return health - damage;
+    }
+    int TakeDamageFromFireball(int damage, int playerHealth)
+    {
+        return playerHealth - damage;
+    }
+}
---
+++ Ahernandez_Hour8__P1/Assets/SideQuest.cs
@@ -0,0 +1,4 @@
+// 1.TakeDamage
+//2. Int
+//3. There is one parameter and the name and type is int damageAmount
+//4. int health = 100; return health - damageAmount;
